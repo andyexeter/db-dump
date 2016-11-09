db24967e7d15f28f3448718790ac8e3d0e41bafa
andyexeter <palmer.andy@gmail.com>
2016-11-09 17:05:51 +0000

    Add config option

--- db-dump.sh
+++ db-dump.sh
@@ -10,17 +10,36 @@ dir=$(dirname `readlink -f $0`)
 # Ensure the gdrive utility is installed
 command -v gdrive >/dev/null 2>&1 || { echo >&2 "gdrive (https://github.com/prasmussen/gdrive) command line utility is required but not installed. Aborting."; exit 1; }
 
+if [ "$1" = 'config' ]; then
+	overwrite="y"
+	if [ -f "$dir/db-dump.conf"  ]; then
+		read -p "Config file already exists, overwrite? (Y/n): " overwrite
+		if [ "$overwrite" != "n" ]; then
+			overwrite="y"
+		fi
+	fi
+
+	if [ "$overwrite" = "y" ]; then
+		echo "Generating config file..."
+		read -p "Enter Database Name: " userdbname
+		read -p "Enter Google Drive Folder ID: " usergdrivefolderid
+
+		cp "$dir/db-dump.conf.dist" "$dir/db-dump.conf"
+		sed -i "s/dbname=\"\"/dbname=\"$userdbname\"/" "$dir/db-dump.conf"
+		sed -i "s/gdrivefolderid=\"\"/gdrivefolderid=\"$usergdrivefolderid\"/" "$dir/db-dump.conf"
+
+		echo "Config file generated."
+	else
+		echo "Aborting."
+	fi
+
+	exit
+fi
+
 # Ensure we have a conf file
-if ! [ -f "$dir/db-dump.conf" ]; then
-	echo "Config file doesn't appear to exist..."
-	read -p "Enter Database Name: " userdbname
-	read -p "Enter Google Drive Folder ID: " usergoogledrivefolderid
-
-	echo "Generating config file..."
-
-	cp "$dir/db-dump.conf.dist" "$dir/db-dump.conf"
-	sed -i "s/dbname=\"\"/dbname=\"$userdbname\"/" "$dir/db-dump.conf"
-	sed -i "s/gdrivefolderid=\"\"/gdrivefolderid=\"$usergoogledrivefolderid\"/" "$dir/db-dump.conf"
+if ! [ -f "$dir/db-dump.conf"  ]; then
+	echo >&2 "Config file does not exist, run '$0 config' to generate it. Aborting."
+	exit 1
 fi
 
 # Load config file
@@ -33,12 +52,12 @@ fi
 
 # Make sure $dbname and $gdrivefolderid aren't empty
 if [ -z "$dbname" ]; then
-	echo "dbname variable not set. Aborting."
+	echo >&2 "dbname variable not set. Aborting."
 	exit 1
 fi
 
 if [ -z "$gdrivefolderid" ]; then
-	echo "gdrivefolderid variable not set. Aborting."
+	echo >&2 "gdrivefolderid variable not set. Aborting."
 	exit 1
 fi
 
@@ -49,7 +68,7 @@ mkdir -p "$dumpdir"
 find "$dumpdir" -type f -name "*.sql.gz" -mtime +"$retentiondays" -print -exec rm "{}" \;
 
 # Zip up any existing export files
-find "$dumpdir" -type f -name "*.sql" -exec gzip "{}" \;
+find "$dumpdir" -type f -name "*.sql" -print -exec gzip "{}" \;
 
 # Dump the live database to a file
 file="$dbname-$(date +$dateformat).sql"
